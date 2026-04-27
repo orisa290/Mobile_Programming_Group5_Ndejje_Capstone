f6e00e603d94abfb3497399722331881ff07fbe1
i26720 <i2672003@gmail.com>
2026-04-27 06:56:21 +0300

    add Material 3 color scheme definitions

---
+++ app/src/main/java/com/ndejje/lostfoundhub/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.ndejje.lostfoundhub.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NavyBlue = Color(0xFF003087)
+val NavyBlueDark = Color(0xFF001540)
+val BrandGold = Color(0xFFC9A227)
+val White = Color(0xFFFFFFFF)
+val LightGrey = Color(0xFFF4F6FB)
+val DarkSurface = Color(0xFF1A1A2E)
+val DarkBackground = Color(0xFF0D1B3E)
+val OnDarkText = Color(0xFFE8F0FE)
+val ErrorRed = Color(0xFFC0392B)
